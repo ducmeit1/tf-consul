972ac069c1e15135b128211bfbab8dcf73540244
ducmeit1 <ducmeit2016@gmail.com>
2020-10-01 12:32:29 +0700

    Update packer and ansible

--- main.tf
+++ main.tf
@@ -41,3 +41,59 @@ module "storage" {
     project = var.project
     location = var.storage_location
 }
+##-----------------------------------
+# This is for testing a standalone vm
+##-----------------------------------
+
+// resource "google_service_account" "vm-consul" {
+//     project = var.project
+//     account_id = "vm-consul"
+//     display_name = "Service account for vm consul"
+// }
+
+// resource "google_service_account_iam_member" "vm-consul-sa" {
+//     service_account_id = google_service_account.vm-consul.id
+//     role = "roles/iam.serviceAccountUser"
+//     member = format("serviceAccount:%s", google_service_account.vm-consul.email)
+// }
+
+// resource "google_compute_instance" "vm_consul" {
+//   name = "vm-consul"
+//   project = var.project
+//   machine_type = "n2d-standard-2"
+//   zone = "asia-east1-c"
+//   tags = ["bastion-access", "consul"]
+//   boot_disk {
+//     initialize_params {
+//       image = "consul-ubuntu1804-lts-5f7568a2-804b-3dd5-451a-1b9f791f25ca"
+//       size = 20
+//     }
+//   }
+
+//   network_interface {
+//     network = module.network.network_self_link
+//     subnetwork = module.network.subnetwork_self_link
+//     network_ip = "10.127.0.3"
+//   }
+
+//   scheduling {
+//     automatic_restart   = false
+//     on_host_maintenance = "TERMINATE"
+//     preemptible         = true
+//   }
+
+//   service_account {
+//     email = google_service_account.vm-consul.email
+//     scopes = ["cloud-platform"]
+//   }
+
+//   metadata = {
+//     enable-oslogin = "TRUE"
+//   }
+
+//   allow_stopping_for_update = true
+
+//   metadata_startup_script = <<EOT
+//     curl -sSO https://dl.google.com/cloudagents/install-monitoring-agent.sh && sudo bash install-monitoring-agent.sh
+//   EOT
+// }
